e68fcf6dad32bc01d6c34ce31311a1a671c696fa
Alexey350 <136724226+Alexey350@users.noreply.github.com>
2023-08-14 19:05:36 +0700

    Add files via upload

--- admin.py
+++ admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import Advertisement
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'auction', 'created_date', 'updated_date']
+    list_display = ['id', 'title', 'description', 'price', 'auction', 'created_date', 'updated_date', 'user', 'image_at']
     list_filter = ['price', 'auction', 'create_at']
     actions = ['make_auction_as_true', 'make_auction_as_false']
     fieldsets = (
         ('Общие', {
-            'fields': ('title', 'description')
+            'fields': ('title', 'description', 'user', 'image')
         }),
         ('Финансы', {
             'fields': ('price', 'auction'),
--- models.py
+++ models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Advertisement(models.Model):
     title = models.CharField(verbose_name='Название', max_length=128)
@@ -10,6 +13,8 @@ class Advertisement(models.Model):
     auction = models.BooleanField('Торг', help_text='Укажите, возможен ли торг')
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
+    image = models.ImageField('изображение', upload_to='advertisements/')
 
     @admin.display(description='дата создания')
     def created_date(self):
@@ -28,6 +33,13 @@ class Advertisement(models.Model):
                 '<span style="color: blue; font-weight: bold;">Обновлено в {}</span>', upd_time 
             )
         return self.update_at.strftime('%d.%m.%Y')
+    
+    @admin.display(description='фотография')
+    def image_at(self):
+            return format_html(
+                '<img src=»{}» width="100" height="111">', self.image
+            )
+
 
 
     first_name = models.CharField(max_length=100)
