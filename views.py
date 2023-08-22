6603cd4041f83b24e12620ade6bb8c6bdcf1f5fd
Alexey350 <136724226+Alexey350@users.noreply.github.com>
2023-08-22 19:15:52 +0700

    Add files via upload

---
+++ views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect, reverse
+from django.core.exceptions import ValidationError
+from .models import Advertisement
+from .forms import AdvertisementForm
+
+def index(request):
+    advertisements = Advertisement.objects.all()
+    context = {'advertisements': advertisements}
+    return render(request, 'index.html', context)
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+def advertisement_post(request):
+    if request.method == 'POST':
+        form = AdvertisementForm(request.POST, request.FILES)
+        if str(form['title'].value())[0] == '?':
+            raise ValidationError('заголовок не может начинаться с вопросительного знака и форма не проходила валидацию')
+        else:
+            if form.is_valid():
+                advertisement = Advertisement(**form.cleaned_data)
+                advertisement.user = request.user
+                advertisement.save()
+                return redirect(reverse('main-page'))
+        
+    form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'advertisement-post.html', context)
